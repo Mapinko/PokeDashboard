81eedde839f3e587e11bca749721f612bc456895
Mari Spinola <120184408+mari-spin@users.noreply.github.com>
2024-01-24 20:40:13 -0300

    fix: responsive banner

--- src/components/MainBanner/index.tsx
+++ src/components/MainBanner/index.tsx
@@ -1,34 +1,28 @@
 import Charizard from "../../assets/Charizard.png"
 import { StyledButton } from '../../styles/buttons'
 import { StyledParagraph, StyledTag, StyledTitle } from '../../styles/typography'
+import { StyledBunner } from './style'
 const MainBanner = () => {
    return (
-      <div>
+      <StyledBunner>
          <div className="flexGrid">
-
             <div className="content">
-               <StyledTag backgroundColor='red'>RPG</StyledTag>
-               <StyledTitle tag="h1" fontWeight={ 400 } fontSize="lg" > Lorem Ipsum PokeWorld </StyledTitle>
+               <StyledTag backgroundColor="red">RPG</StyledTag>
+               <StyledTitle tag="h1" fontWeight={ 400 } fontSize="lg">Lorem Ipsum Pokéworld</StyledTitle>
+               <div className="priceBox">
+                  <StyledParagraph fontSize="md" opacity={ 1 }>
+                     <strong>R$ 299,00</strong>
+                  </StyledParagraph>
+                  <StyledButton buttonStyle="solid" buttonSize="lg">
+                     Buy
+                  </StyledButton>
+               </div>
             </div>
-
-            <div className="priceBox">
-               <StyledParagraph fontSize='md' opacity={ 1 }>
-                  <strong>
-                     299,00
-                  </strong>
-               </StyledParagraph>
-               <StyledButton buttonStyle='solid' buttonSize='lg'>
-                  Comprar
-               </StyledButton>
-            </div>
-
-
-            <div className="imageGrid" >
+            <div className="imageGrid">
                <img src={ Charizard } alt="Charizard" />
             </div>
-
          </div>
-      </div>
+      </StyledBunner>
    )
 }
 
