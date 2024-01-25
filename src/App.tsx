af836a21e5056c5354a62a83a0d9aff00d0587e2
Mari Spinola <120184408+mari-spin@users.noreply.github.com>
2024-01-25 01:36:53 -0300

    fix: wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App () {
   return (
     <StyledMainWrapper>
       <Menu />
-      <div>
+      <div className='main-content'>
         <Header />
         <MainBanner />
         <GameList />
--- src/styles/wrapper.ts
+++ src/styles/wrapper.ts
@@ -3,4 +3,13 @@ import styled from 'styled-components'
 export const StyledMainWrapper = styled.div`
    display: flex;
    gap: 1rem;
+
+   .main-content{
+      display: inline-flex;
+      flex-direction: column;
+      gap: 1.5rem;
+
+      padding: 0 1rem;
+      
+   }
 `
